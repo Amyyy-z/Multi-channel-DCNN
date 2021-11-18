5fd63edf4e4021f1d3301b4a65752b05c1025464
Amy Zhang <78408435+Amyyy-z@users.noreply.github.com>
2021-11-19 00:02:32 +1100

    Update VGG.py

--- VGG.py
+++ VGG.py
@@ -8,6 +8,8 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models, Model, Sequential, regularizers
 
+# VGG models construction
+
 cfgs = {
     'vgg8': [64,'M',128,'M',256,'M',512,'M',512,'M'],
     'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
